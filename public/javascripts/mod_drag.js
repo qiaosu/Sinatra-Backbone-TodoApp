6b7b6b1b5b30520afca95d14cd11d481ed0dd662
Qiao Su <tracy.xin.li@gmail.com>
2012-08-28 00:30:03 +0800

    drag mouseEvents

--- public/javascripts/mod_drag.js
+++ public/javascripts/mod_drag.js
@@ -1,86 +1,221 @@
 !function(name, definition) {
     _[name] = definition();
-}('Drag', function(opt) {
-    function Drag(opt) {
-        var clearTimeout = window.clearTimeout,
-            setTimeout = window.setTimeout,
-            abs = Math.abs,
-            doc = $(document),
-            start, 
-            start_time = 0,
-            isMove = true,
-            preventDefault = typeof opt.preventDefault === "undefined" ? true : false,
-            whenDragStart = opt.whenDragStart || function() {}, 
-            whenDraging = opt.whenDraging || function() {}, 
-            whenDragEnd = opt.whenDragEnd || function() {}, 
-            whenClick = opt.whenClick || function() {}, 
-            handler = this.handler = $(opt.handler);
-
-        this.dragHandle = function(e) {
-            if (!isMove || e.button == 2) {
-                return;
+}('Drag', function() {
+    function Drag() {
+        var drag = {
+            element:        "",
+            element_move:   "",
+            constraint:     "",
+            sliding:        false,
+            pagex: {
+                start:      0,
+                end:        0
+            },
+            left: {
+                start:      0,
+                end:        0
+            },
+            time: {
+                start:      0,
+                end:        0
+            },
+            touch:          false,
+            ease:           "easeOutExpo"
+        },
+        dragevent = {
+            down:       "mousedown",
+            up:         "mouseup",
+            leave:      "mouseleave",
+            move:       "mousemove"
+        },
+        mousedrag = {
+            down:       "mousedown",
+            up:         "mouseup",
+            leave:      "mouseleave",
+            move:       "mousemove"
+        },
+        touchdrag = {
+            down:       "touchstart",
+            up:         "touchend",
+            leave:      "mouseleave",
+            move:       "touchmove"
+        };
+        
+        this.createPanel = function(drag_object, move_object, constraint, touch) {
+            drag.element        = drag_object;
+            drag.element_move   = move_object;
+            
+            if ( constraint != null && constraint != "") {
+                drag.constraint = constraint;
+            } else {
+                drag.constraint = false;
             }
-            if (preventDefault) {
+            if ( touch) {
+                drag.touch = touch;
+            } else {
+                drag.touch = false;
+            }
+            console.log("TOUCH" + drag.touch);
+            if (drag.touch) {
+                dragevent = touchdrag;
+            } else {
+                dragevent = mousedrag;
+            }
+            
+            makeDraggable(drag.element, drag.element_move);
+        }
+        
+        this.updateConstraint = function(constraint) {
+            console.log("updateConstraint");
+            drag.constraint = constraint;
+        }
+        
+        var makeDraggable = function(drag_object, move_object) {
+            
+            $(drag_object).on(dragevent.down, {element: move_object, delement: drag_object}, onDragStart);
+            $(drag_object).on(dragevent.up, {element: move_object, delement: drag_object}, onDragEnd);
+            $(drag_object).on(dragevent.leave, {element: move_object, delement: drag_object}, onDragLeave);
+            
+        }
+        this.cancelSlide = function(e) {
+            $(drag.element).off(dragevent.move, onDragMove);
+            return true;
+        }
+        var onDragLeave = function(e) {
+            $(e.data.delement).off(dragevent.move, onDragMove);
+            if (!drag.touch) {
                 e.preventDefault();
             }
-            isMove = false;
-            start = [e.pageX, e.pageY];
-            start_time = +new Date();
-            doc.mousemove(draging).mouseup(dragEnd);
-        };
-        function dragStart(e) {
-            whenDragStart(start);
-            handler.mouseover(dragDisable).mouseout(dragDisable).click(clickDisable);
+            e.stopPropagation();
+            if (drag.sliding) {
+                drag.sliding = false;
+                dragEnd(e.data.element, e.data.delement, e);
+                return false;
+            } else {
+                return true;
+            }
         }
-        function draging(e) {
+        
+        var onDragStart = function(e) {
+            dragStart(e.data.element, e.data.delement, e);
+            if (!drag.touch) {
+                e.preventDefault();
+            }
+            e.stopPropagation();
+            return true;
+        }
+        
+        var onDragEnd = function(e) {
+            if (!drag.touch) {
+                e.preventDefault();
+            }
+            e.stopPropagation();
+            if (drag.sliding) {
+                drag.sliding = false;
+                dragEnd(e.data.element, e.data.delement, e);
+                return false;
+            } else {
+                return true;
+            }
+        }
+        var onDragMove = function(e) {
+            dragMove(e.data.element, e);
             e.preventDefault();
-            if (!isMove) {
-                if (abs(e.pageX - start[0]) > 3 || abs(e.pageY - start[1]) > 3 || +new Date() - start_time > 500) {
-                    isMove = true;
-                    dragStart(e);
-                } else {
-                    return;
+            e.stopPropagation();
+            return false;
+        }
+        var dragStart = function(elem, delem, e) {
+            if (drag.touch) {
+                console.log("IS TOUCH")
+                $(elem).css('-webkit-transition-duration', '0');
+                drag.pagex.start = e.originalEvent.touches[0].screenX;
+            } else {
+                drag.pagex.start = e.pageX;
+            }
+            drag.left.start = getLeft(elem);
+            drag.time.start = new Date().getTime();
+            
+            $(elem).stop();
+            $(delem).on(dragevent.move, {element: elem}, onDragMove);
+
+        }
+        var dragEnd = function(elem, delem, e) {
+            $(delem).off(dragevent.move, onDragMove);
+            dragMomentum(elem, e);
+        }
+        var dragMove = function(elem, e) {
+            drag.sliding = true;
+            if (drag.touch) {
+                drag.pagex.end = e.originalEvent.touches[0].screenX;
+            } else {
+                drag.pagex.end = e.pageX;
+            }
+            drag.left.end = getLeft(elem);
+            $(elem).css('left', -(drag.pagex.start - drag.pagex.end - drag.left.start));
+            
+        }
+        var dragMomentum = function(elem, e) {
+            var drag_info = {
+                    left:           drag.left.end,
+                    left_adjust:    0,
+                    change: {
+                        x:          0
+                    },
+                    time:           (new Date().getTime() - drag.time.start) * 10,
+                    time_adjust:    (new Date().getTime() - drag.time.start) * 10
+                },
+                multiplier = 3000;
+                
+            if (drag.touch) {
+                multiplier = 6000;
+            }
+            
+            drag_info.change.x = multiplier * (Math.abs(drag.pagex.end) - Math.abs(drag.pagex.start));
+            
+            
+            drag_info.left_adjust = Math.round(drag_info.change.x / drag_info.time);
+            
+            drag_info.left = Math.min(drag_info.left + drag_info.left_adjust);
+            
+            if (drag.constraint) {
+                if (drag_info.left > drag.constraint.left) {
+                    drag_info.left = drag.constraint.left;
+                    if (drag_info.time > 5000) {
+                        drag_info.time = 5000;
+                    }
+                } else if (drag_info.left < drag.constraint.right) {
+                    drag_info.left = drag.constraint.right;
+                    if (drag_info.time > 5000) {
+                        drag_info.time = 5000;
+                    }
                 }
             }
-            var prev = start;
-            start = [e.pageX, e.pageY];
-            whenDraging(prev, start);
-        }
-        function dragEnd(e) {
-            doc.unbind("mousemove", draging).unbind("mouseup", dragEnd);
-            if (isMove) {
-                handler.unbind("mouseover", dragDisable).unbind("mouseout", dragDisable);
-                whenDragEnd(start);
-            } else {
-                whenClick(e);
-                isMove = true;
+            
+            $(elem).triggerHandler("DRAGUPDATE", [drag_info]);
+
+            
+            if (drag_info.time > 0) {
+                if (drag.touch) {
+                    //$(elem).css('-webkit-transition-property', 'left');
+                    //$(elem).css('-webkit-transition-duration', drag_info.time);
+                    //$(elem).css('left', drag_info.left);
+                    
+                    //$(elem).animate({"left": drag_info.left}, drag_info.time, "easeOutQuad");
+                    $(elem).animate({"left": drag_info.left}, drag_info.time, "easeOut");
+                    //$(elem).css('webkitTransition', '');
+                    //$(elem).css('webkitTransition', '-webkit-transform ' + drag_info.time + 'ms cubic-bezier(0.33, 0.66, 0.66, 1)');
+                    //$(elem).css('webkitTransform', 'translate3d(' + drag_info.left + 'px, 0, 0)');
+
+                } else {
+                    $(elem).animate({"left": drag_info.left}, drag_info.time, drag.ease);
+                }
             }
+            
         }
-        function dragDisable() {
-            return false;
-        }
-        function clickDisable() {
-            handler.unbind("click", clickDisable);
-            return false;
+        var getLeft = function(elem) {
+            return parseInt($(elem).css('left').substring(0, $(elem).css('left').length - 2), 10);
         }
     }
-
-    Drag.prototype = {
-        enable: function() {
-            if (!this.enabled) {
-                this.handler.mousedown(this.dragHandle);
-                this.enabled = true;
-            }
-            return this;
-        },
-        disable: function() {
-            if (this.enabled) {
-                this.handler.unbind("mousedown", this.dragHandle);
-                this.enabled = false;
-            }
-            return this;
-        }
-    };
 
     return Drag;
 });
--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -2,9 +2,14 @@ window.TimelineView = Backbone.View.extend({
 	el: $('#timeline-container'),
 	events: {},
 	initialize: function(){
-		this.drag = new _.Drag({
-			'handler': $('.timenav', this.el)
-		})
+		// MAKE TIMELINE DRAGGABLE/TOUCHABLE
+		this.drag = new _.Drag();
+		this.drag.createPanel('#timeline-container .navigation', $(this.el).find('.timenav'), {
+			left: 0,
+			right: -562.2222222222022,
+			right_max: 0,
+			right_min: -1282.2222222222022
+		}, false);
 		console.log('timeline.');
 	}
 })
