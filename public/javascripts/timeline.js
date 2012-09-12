9ce46a84c7edc288fa1d093dc21016d36c323b2e
Qiaosu <tracy.xin.li@gmail.com>
2012-09-12 17:32:47 +0800

    license

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -1,3 +1,15 @@
+/*!
+	Thank for:
+	TimelineJS
+	Version 2.17
+	Designed and built by Zach Wise at VéritéCo
+
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+	
+*/
+
 window.TimelineView = Backbone.View.extend({
 	el: $('#timeline-container'),
 	events: {
